f8f5a9afab07a29265805520eb3bc31c65f0b1a8
Vagif <np.info.mail@gmail.com>
2023-07-21 18:31:26 +0400

    21.07.2023

--- main.py
+++ main.py
@@ -19,15 +19,29 @@ driver.maximize_window()
 
 category = driver.find_element(By.XPATH, "//span[@id='catnav-primary-link-10923']")
 category.click()
+time.sleep(3)
+pagination = driver.find_elements(By.XPATH, '//span[@class="screen-reader-only"]')
+print(f'page len:\t{len(pagination)} | {pagination[3].text}')
 # seller XPATH: //span[@data-ad-label="Ad by Etsy seller"]
 
 # item = driver.find_element(By.XPATH, "searchbox-searchbutton")
 
 
-def print_hi(name):
-    print(f'Height: {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def find_current_page(pagination_element: list):
+    try:
+        pag_list = list(map(lambda pg: pagination_element[pagination_element.index(pg)+1].text if pg.text == 'Current page' else None, pagination_element))
+        cur_page_try = list(filter(lambda fpg: fpg is not None, pag_list))[0]
+        print(f'cr+1:\t{cur_page_try}')
+        cur_p = int(cur_page_try.split("Page ")[1])-1
+    except ValueError:
+        pag_list = list(map(lambda pg: pagination_element[pagination_element.index(pg)-1].text if pg.text == 'Current page' else None, pagination_element))
+        cur_page_try = list(filter(lambda fpg: fpg is not None, pag_list))[0]
+        print(f'cr-1:\t{cur_page_try}')
+        cur_p = int(cur_page_try.split("Page ")[1])+1
+
+    return cur_p
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi("Hi")
+    print(f'current page is:\t{find_current_page(pagination_element=pagination)}')
