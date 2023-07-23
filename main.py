0f7946456a4c670aa3598e1b107016435ee020f4
Vagif <np.info.mail@gmail.com>
2023-07-24 01:06:15 +0400

    24.2008

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+import json
 
 # main variables
 s = Service('./chromedriver/chromedriver')
@@ -22,21 +23,27 @@ category.click()
 time.sleep(3)
 pagination = driver.find_elements(By.XPATH, '//span[@class="screen-reader-only"]')
 print(f'page len:\t{len(pagination)} | {pagination[3].text}')
-# seller XPATH: //span[@data-ad-label="Ad by Etsy seller"]
-
-# item = driver.find_element(By.XPATH, "searchbox-searchbutton")
+# sellers = driver.find_elements(By.XPATH, "//span[@data-ad-label="Ad by Etsy seller"]")
+items = driver.find_elements(By.XPATH, "//div[@class='wt-height-full']")
+list(map(lambda fit: print(json.loads(str(fit.get_attribute('data-appears-batch-options')))), items))
+items = list(filter(lambda fit: json.loads(str(fit.get_attribute('data-appears-batch-options')))['total_items'] > 16, items))
+print(f'items quota: {len(items)}')
+wanted_product_list = [
+    {
+        'product': 'Dachshund Sausage Dog Bag Charm / Keyring / Hanging / Gift Friend Mum Special',
+        'seller': 'CraftyKeek'
+    }
+]
 
 
 def find_current_page(pagination_element: list):
     try:
         pag_list = list(map(lambda pg: pagination_element[pagination_element.index(pg)+1].text if pg.text == 'Current page' else None, pagination_element))
         cur_page_try = list(filter(lambda fpg: fpg is not None, pag_list))[0]
-        print(f'cr+1:\t{cur_page_try}')
         cur_p = int(cur_page_try.split("Page ")[1])-1
     except ValueError:
         pag_list = list(map(lambda pg: pagination_element[pagination_element.index(pg)-1].text if pg.text == 'Current page' else None, pagination_element))
         cur_page_try = list(filter(lambda fpg: fpg is not None, pag_list))[0]
-        print(f'cr-1:\t{cur_page_try}')
         cur_p = int(cur_page_try.split("Page ")[1])+1
 
     return cur_p
@@ -45,3 +52,5 @@ def find_current_page(pagination_element: list):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(f'current page is:\t{find_current_page(pagination_element=pagination)}')
+    time.sleep(3)
+    driver.close()
