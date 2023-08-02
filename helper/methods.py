2184f054bdd875d362ee3d464da4e4b21954fe67
Vagif <np.info.mail@gmail.com>
2023-08-02 22:06:31 +0400

    02.08.2023

--- helper/methods.py
+++ helper/methods.py
@@ -9,3 +9,42 @@ from selenium.webdriver.common.action_chains import ActionChains
 from waitress import serve
 import json
 
+
+def selenium_method(id_element):
+    s = Service('./chromedriver/chromedriver')
+    url_ws = "https://www.etsy.com/"
+    options = Options()
+    options.add_experimental_option('detach', True)
+    driver = webdriver.Chrome(service=s, options=options)
+    a = ActionChains(driver)
+    # params
+    driver.get(url_ws)
+    driver.maximize_window()
+
+    category = driver.find_element(By.XPATH, "//span[@id='catnav-primary-link-10923']")
+    category.click()
+    while True:
+        time.sleep(3)
+        pagination = driver.find_elements(By.XPATH, '//span[@class="screen-reader-only"]')
+        last_pagination = pagination[-2].text
+        prev_last_pagination = pagination[-3].text
+        print(f'page len:\t{len(pagination)} | last: {last_pagination}')
+        items = driver.find_elements(By.XPATH, "//div[@class='wt-height-full']")
+        items = list(filter(lambda fit: json.loads(str(fit.get_attribute('data-appears-batch-options')))['total_items'] > 16, items))
+        item_title_list = list(map(lambda fit: {
+            'title': str(fit.find_element(By.TAG_NAME, "a").get_attribute('title')),
+            # 'element': fit,
+            'data-listing-id': str(fit.find_element(By.TAG_NAME, "a").get_attribute('data-listing-id'))
+        }, items))
+        print(f'\nList:\n--------------------\n{item_title_list}\n--------------------\n')
+        print(f'items quota: {len(items)} | current url: {driver.current_url}')
+        item_title_list = list(filter(lambda f: f['data-listing-id'] == id_element, item_title_list))
+        result_count = len(item_title_list)
+        print(f'{result_count} result fount')
+        if result_count > 0 or prev_last_pagination == 'Current page':
+            break
+        elif result_count == 0 and prev_last_pagination != 'Current page':
+            pagination[-1].click()
+
+    driver.close()
+    return item_title_list
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from waitress import serve
 import json
+from helper.methods import *
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 # app.config['UPLOAD_FOLDER'] = upload_folder
@@ -17,38 +18,40 @@ app.secret_key = "etsybot"
 @app.route('/')
 def home():
     # main variables
-    s = Service('./chromedriver/chromedriver')
-    url_ws = "https://www.etsy.com/"
-    options = Options()
-    options.add_experimental_option('detach', True)
-    driver = webdriver.Chrome(service=s, options=options)
-    a = ActionChains(driver)
-    # params
-    driver.get(url_ws)
-    driver.maximize_window()
-
-    category = driver.find_element(By.XPATH, "//span[@id='catnav-primary-link-10923']")
-    category.click()
-    time.sleep(3)
-    pagination = driver.find_elements(By.XPATH, '//span[@class="screen-reader-only"]')
-    print(f'page len:\t{len(pagination)}')
-    # sellers = driver.find_elements(By.XPATH, '//span[@data-ad-label="Ad by Etsy seller"]')
-    # sellers = driver.find_elements(By.XPATH, '//span[@class="wt-text-title-small"]')  # big page
-    # phrase = 'Personalized camera strap with blue world map design. Comfortable and safe strap - Best gift for photographer. Purple and padded strap'
-    # seller_name = 'InTePro'
-    items = driver.find_elements(By.XPATH, "//div[@class='wt-height-full']")
-    items = list(filter(lambda fit: json.loads(str(fit.get_attribute('data-appears-batch-options')))['total_items'] > 16, items))
-    item_title_list = list(map(lambda fit: {
-        'title': str(fit.find_element(By.TAG_NAME, "a").get_attribute('title')),
-        # 'element': fit,
-        'data-listing-id': str(fit.find_element(By.TAG_NAME, "a").get_attribute('data-listing-id'))
-    }, items))
-    print(f'\nList:\n--------------------\n{item_title_list}\n--------------------\n')
-    wanted_product_list = ["398801525"]
-    print(f'items quota: {len(items)}')
-    item_title_list = list(filter(lambda f: f['data-listing-id'] in wanted_product_list, item_title_list))
-
-    return render_template('index.html', data=item_title_list)
+    id_list = ['398801525']
+    result = list(map(lambda id_item: selenium_method(id_item), id_list))
+    # s = Service('./chromedriver/chromedriver')
+    # url_ws = "https://www.etsy.com/"
+    # options = Options()
+    # options.add_experimental_option('detach', True)
+    # driver = webdriver.Chrome(service=s, options=options)
+    # a = ActionChains(driver)
+    # # params
+    # driver.get(url_ws)
+    # driver.maximize_window()
+    #
+    # category = driver.find_element(By.XPATH, "//span[@id='catnav-primary-link-10923']")
+    # category.click()
+    # time.sleep(3)
+    # pagination = driver.find_elements(By.XPATH, '//span[@class="screen-reader-only"]')
+    # print(f'page len:\t{len(pagination)}')
+    # # sellers = driver.find_elements(By.XPATH, '//span[@data-ad-label="Ad by Etsy seller"]')
+    # # sellers = driver.find_elements(By.XPATH, '//span[@class="wt-text-title-small"]')  # big page
+    # # phrase = 'Personalized camera strap with blue world map design. Comfortable and safe strap - Best gift for photographer. Purple and padded strap'
+    # # seller_name = 'InTePro'
+    # items = driver.find_elements(By.XPATH, "//div[@class='wt-height-full']")
+    # items = list(filter(lambda fit: json.loads(str(fit.get_attribute('data-appears-batch-options')))['total_items'] > 16, items))
+    # item_title_list = list(map(lambda fit: {
+    #     'title': str(fit.find_element(By.TAG_NAME, "a").get_attribute('title')),
+    #     # 'element': fit,
+    #     'data-listing-id': str(fit.find_element(By.TAG_NAME, "a").get_attribute('data-listing-id'))
+    # }, items))
+    # print(f'\nList:\n--------------------\n{item_title_list}\n--------------------\n')
+    # wanted_product_list = ["398801525"]
+    # print(f'items quota: {len(items)}')
+    # item_title_list = list(filter(lambda f: f['data-listing-id'] in wanted_product_list, item_title_list))
+    # driver.close()
+    return render_template('index.html', data=result)
 
 
 if __name__ == '__main__':
